5420b67fe38135161e4303d3d8060a69a5a4e708
Cary Carter <ccarterdev@gmail.com>
2024-02-15 16:45:24 -0800

    [2 FILE(s)] .gitignore status/config.py

--- status/config.py
+++ status/config.py
@@ -1,1 +1,8 @@
+from os.path import abspath, join, dirname
+
 POLL_EVERY = 5
+
+STATUS_FILES = {
+    'liberlife': join(abspath(dirname(__file__)), 'life.liberfy.ai.log'),
+    'open2fa': join(abspath(dirname(__file__)), 'open2fa.liberfy.ai.log'),
+}
