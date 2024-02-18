c23d6dddecf4196fa691f5e4d40a29856c21f09e
Cary Carter <ccarterdev@gmail.com>
2024-02-18 02:50:18 -0800

    fixed cachebust links

--- cachebust.sh
+++ cachebust.sh
@@ -5,4 +5,4 @@ timestamp=$(date +%s)
 
 # This command carefully targets href and src attributes to append the timestamp,
 # avoiding unintended replacements elsewhere.
-sed -i.bak -E "s/(href|src)=\"([^\"]+\.[a-zA-Z0-9]+)(\?[^\"]*)?\"/\1=\"\2?${timestamp}\"/g" html/index.html && rm html/index.html.bak
+sed -i.bak -E "s/(href|src)=\"([^\"]+\.(png|ico|js|css|webmanifest))(\?[^\"]*)?\"/\1=\"\2?${timestamp}\"/g" html/index.html && rm html/index.html.bak
